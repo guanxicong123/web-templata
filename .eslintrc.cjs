efa29ba24bbb7758378ace629fd7caa48348a85b
Guan <616833273@qq.com>
2023-06-17 16:10:12 +0800

    feat：添加api && tsconfig配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,14 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  // 指定如何解析语法
+  parser: 'vue-eslint-parser',
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
